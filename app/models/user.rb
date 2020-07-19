f527b06b781a678e92f4597442322e792b897a25
Ciel <39741348+tomoki-akatsu@users.noreply.github.com>
2020-07-19 17:08:11 +0900

    [Add] Usersテーブルの作成

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # passwordをbcryptでハッシュ化してpassword_digestに入れるために必要
+  has_secure_password
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+end
