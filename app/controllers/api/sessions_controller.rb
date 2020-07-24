75998b15d6b0b4ffb6a5ccd2692e4df0b4d31515
Ciel <39741348+tomoki-akatsu@users.noreply.github.com>
2020-07-24 16:00:08 +0900

    [Modify] コメントの追加

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -4,8 +4,10 @@ class Api::SessionsController < ApiController
 
     # emailを持つユーザーが存在し、passwordが一致してる場合のみjwtを返す
     if user&.authenticate(session_params[:password])
-      # jwtを生成
+      # jwtを生成(token_provider.rbのcallメソッドを実行)
       token = Jwt::TokenProvider.(user_id: user.id)
+      # user_serializer.rbで定義していないtokenを追加するため、
+      # JSONオブジェクトを作成後にtokenをdeepmergeで結合している
       render json: ActiveModelSerializers::SerializableResource.new(user, serializer: UserSerializer).as_json.deep_merge(user: { token: token })
     else
       render json: { error: "メールアドレスまたはパスワードに誤りがあります。" }, status: 401
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,6 +2,7 @@ class Api::UsersController < ApiController
   def create
     user = User.new(user_params)
     if user.save
+      # user_serializer.rb内のattributesで定義したカラムのみをJSONオブジェクト内に入れる
       render json: user, serializer: UserSerializer
     else
       error_message = { error: { messages: [ "Email has already been taken" ] } }
@@ -11,6 +12,7 @@ class Api::UsersController < ApiController
 
   private
     def user_params
+      # 想定しているパラメータのみを受け取る
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
