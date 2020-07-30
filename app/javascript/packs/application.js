bc90ef95b80e6d0825b3496176f5b780bcaf1a6e
Ciel <skyblue.cfsr0918@gmail.com>
2020-07-31 02:10:08 +0900

    [add] Vuetifyの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,23 @@ require("@rails/ujs").start();
 require("@rails/activestorage").start();
 require("channels");
 
+import Vue from "vue";
+import App from "../app.vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+Vue.use(Vuetify);
+const vuetify = new Vuetify();
+
+document.addEventListener("DOMContentLoaded", () => {
+  const app = new Vue({
+    vuetify,
+    render: (h) => h(App),
+  }).$mount();
+  document.body.appendChild(app.$el);
+
+  console.log(app);
+});
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "home#index"
   namespace :api do
     # RestAPIのルーティングを簡単に書くための記法
     # 今回はcreateのみ使用したいのでonlyオプションで指定する
