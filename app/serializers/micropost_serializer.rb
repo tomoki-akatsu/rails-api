617ea2d50b6da49d459a8b5637bb676d6f38fbd8
Ciel <skyblue.cfsr0918@gmail.com>
2020-12-09 00:14:54 +0900

    [modify] micropostsのレスポンスに含まれているuserの項目を制限するように変更

--- app/serializers/micropost_serializer.rb
+++ app/serializers/micropost_serializer.rb
@@ -1,4 +1,6 @@
 class MicropostSerializer < ActiveModel::Serializer
   # JSONで返却するカラムを指定
   attributes :id, :content, :created_at, :updated_at, :user
+  # レスポンス内の項目をuser_serializerで定義した項目を返却するようにする
+  belongs_to :user
 end
